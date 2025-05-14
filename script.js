1e6de12c3389ffd4339c6478b44e95019b3c115b
iamLucasDev <lucas110@hotmail.com.br>
2025-05-13 21:50:33 -0300

    calculadora

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// vai aparecer os números no display
 function insert(num){
-    let res = document.getElementById("display").innerHTML +=`${num}`
+  let res = document.getElementById("display").innerHTML += `${num}`;
+}
+
+// Vai limpar todos os números dos display
+function clean(){
+   let limpar = document.getElementById("display").innerHTML = "";
+}
+
+// Vai apagar o último o número do display
+function back(){
+   let res = document.getElementById("display");
+   res.innerHTML =  res.innerHTML.slice(0, -1);
+}
+
+// Vai calcular os números
+function calcular() {
+    let res = document.getElementById("display");
+    if (res.innerHTML !== "") {
+        try {
+            // Substitui 'x' por '*' para fazer multiplicação funcionar
+            let resultado = eval(res.innerHTML.replace(/x/g, '*'));
+            res.innerHTML = resultado;
+        } catch (e) {
+            res.innerHTML = "Erro";
+        }
+    } else {
+        res.innerHTML = "Erro";
+    }
 }
